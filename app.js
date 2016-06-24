6e0121da7c2cc76b7d793ce9abca07e50447b1e8
Stacy <swangny@gmail.com>
2016-06-24 15:09:01 -0400

    changed content of app.js

--- app.js
+++ app.js
@@ -28,5 +28,5 @@ app.get("/random", function(req, res){
 });
 
 app.listen(3000, function(){
-	console.log('Inspiration app listening on port 3000...');
+	console.log('listening on port 3000...');
 });
